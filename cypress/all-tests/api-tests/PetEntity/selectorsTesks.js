69c87d3475f48956a01eacd6dfd87793951af2bb
ilyaaleks <ilya.aleks.2011@gmail.com>
2019-10-27 21:17:21 +0300

    Add celectors

---
+++ cypress/all-tests/api-tests/PetEntity/selectorsTesks.js
@@ -0,0 +1,30 @@
+import Chance from 'chance'
+import {createPet, deletePet, getPetById, updatePet} from "../../../service/petService"
+import {DATA_OPTIONS, getPetRequestData} from "../../../utils/requestsDataGenerator";
+import {API_URL} from "../../../service/apiSettings";
+import {PET_LIMIT} from "../../../utils/limits";
+
+describe('Tests https://techstepacademy.com/training-ground', () => {
+
+    it(("Selectors"), () => {
+        cy.visit("https://techstepacademy.com/training-ground/");
+        cy.get('[name^="i2_"]')
+        cy.xpath("//*[contains(@name,'i2_')]")
+
+        cy.get('input[id="ipt1"]')
+        cy.xpath("//input[@id='ipt1']")
+
+        cy.get('button').contains('Button1')
+        cy.xpath("//button[contains(text(),'Button1')]")
+
+        cy.get('h2').contains('A dropdown of three things').next('select')
+        cy.xpath("//h2[contains(text(),'A dropdown of three things')]/following::select")
+
+        cy.get('option').contains('Beets').siblings('option')
+        cy.xpath("//option [contains(text(),'Beets')]/preceding-sibling::option ")
+
+        cy.get('div[class="sqs-block-content"] > p')
+        cy.xpath('//div[@class="sqs-block-content"]/child::p')
+    })
+
+})
